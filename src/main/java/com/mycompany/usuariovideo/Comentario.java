c8328711135a052c05951252c57ffd75f036f7de
joseg <joseg@192.168.0.229>
2025-04-08 00:35:12 +0100

    para el profe sensei

---
+++ src/main/java/com/mycompany/usuariovideo/Comentario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.usuariovideo;
+
+/**
+ *
+ * @author joseg
+ */
+public class Comentario {
+    private String comentario;
+
+    // Constructor que recibe el comentario
+    public Comentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    // Método para obtener el comentario
+    public String getComentario() {
+        return comentario;
+    }
+}
